dc8cbf45fbb731b7ba2f1e38163d9e5f90aebc33
em4n0n <58372945+em4n0n@users.noreply.github.com>
2025-03-14 20:05:52 -0700

    update staticfiles dirs

--- littlelemon/littlelemon/settings.py
+++ littlelemon/littlelemon/settings.py
@@ -122,6 +122,10 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATICFILES_DIRS = [
+    "restaurant/static",
+]
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
