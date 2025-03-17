01a919d1b96fcce6e17b28f23ec797be9286495d
em4n0n <58372945+em4n0n@users.noreply.github.com>
2025-03-16 20:20:47 -0700

    return the render function and pass request and bookings.html as a string

--- littlelemon/restaurant/views.py
+++ littlelemon/restaurant/views.py
@@ -28,7 +28,7 @@ def bookings(request):
     date = request.GET.get('date', datetime.today().date())
     bookings = Booking.objects.all()
     booking_json = serializers.serialize('json', bookings)
-
+    return render(request, "bookings.html", {"bookings": booking_json})
 
 def menu(request):
     menu_data = Menu.objects.all()
